70cfb7d19fa6bea68aa3c1df90c19d9f5e786197
upils <upils@bigboss>
2016-12-02 00:22:01 +0100

    Gestionnaire d'upload de fichier.

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ $(document).on("keypress", function (e) {
 });
 
 
-function download() {
+function downloadFile() {
   var filename = "test";
   var text = "test\r\ntest";
   var element = document.createElement('a');
@@ -17,4 +17,25 @@ function download() {
   element.click();
 
   document.body.removeChild(element);
+}
+
+function fileGeter() {
+  var selectedFile = document.getElementById('fileSelector').files[0];
+  var reader = new FileReader();
+  reader.onload = function(event) {
+    var text = event.target.result;
+    var lines = text.split(/[\r\n]+/g);
+    var ability = "";
+    var key ="";
+    var pair;
+
+    for(var i = 0; i < lines.length; i++) {
+      pair = lines[i].split(/=/g);
+      ability = pair[0];
+      key = pair[1];
+      console.log(ability);
+      console.log(key);
+    }
+  };
+  reader.readAsText(selectedFile, 'UTF-8');
 }
