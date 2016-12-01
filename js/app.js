be669e63fd0535990583e5c09fedb405765913bb
upils <upils@bigboss>
2016-12-01 23:01:58 +0100

    Ajout de la fonction de generation de lien de téléchargement.

--- js/app.js
+++ js/app.js
@@ -2,3 +2,19 @@ $(document).foundation()
 $(document).on("keypress", function (e) {
     console.log(String.fromCharCode(e.which));
 });
+
+
+function download() {
+  var filename = "test";
+  var text = "test\r\ntest";
+  var element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute('download', filename);
+
+  element.style.display = 'none';
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+}
